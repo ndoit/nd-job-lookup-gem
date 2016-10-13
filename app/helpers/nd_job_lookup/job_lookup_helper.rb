989c838f461b3a90b88dddb46f7f3e0d263d5109
Kingdon Barrett <kbarret8@nd.edu>
2016-10-13 16:06:31 -0400

    pending specs for the partial story

---
+++ app/helpers/nd_job_lookup/job_lookup_helper.rb
@@ -0,0 +1,4 @@
+module NdJobLookup
+  module JobLookupHelper
+  end
+end
---
+++ spec/controllers/nd_job_lookup/job_lookup_controller_spec.rb
@@ -0,0 +1,14 @@
+module NdJobLookup
+  RSpec.describe JobLookupController, type: :controller do
+    render_views
+    routes { NdJobLookup::Engine.routes }
+
+    describe 'GET #new' do
+      it "renders the search form as a partial" do
+        get :new
+        pending
+        expect(response).to render_template(partial: '_job_search')
+      end
+    end
+  end
+end
