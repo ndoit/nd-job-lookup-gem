a030c55c339cf7ff16d87f3c648280076e7be790
Kingdon Barrett <kbarret8@nd.edu>
2016-10-21 11:03:04 -0400

    neither employee-lookup nor job-lookup currently..
    provide a working standalone implementation that can pass any kind of
    browser testing, should both be rebuilt to have out-of-the-box working
    search and show controllers and the border between what they provide and
    what the host application must do should be re-evaluated.
    
    (currently the host is expected by employee-lookup to implement handler
    to control what is done with employee information after employee lookup
    is performed, and in our implementation actually reaches into this gem
    when EmployeesController#show builds the params for and interrogates the
    model NdJobLookup::HrpyEmployeeJob directly via the #search method, to
    retrieve the Jobs data for server-side rendering and display.)
    
    Not happy with this, so it's probably time to cut a release for both
    gems (since it works) and build a better usage in the example app.

--- spec/features/javascript_search_spec.rb
+++ spec/features/javascript_search_spec.rb
@@ -3,4 +3,12 @@ feature 'JavaScript search controller' do
     visit '/job-lookup/search'
     expect(page).not_to have_errors
   end
+  it 'should use employee-lookup to retrieve a single job result', js: true do
+    pending "the spec for employee-lookup-gem has been broken"
+    raise Error
+  end
+  it 'should find a person with more than one active job', js: true do
+    pending "the job-lookup is implemented abstractly and does not provide show"
+    raise Error
+  end
 end
