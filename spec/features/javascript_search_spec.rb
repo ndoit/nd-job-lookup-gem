4be852f06eb2819d5077b73784dee803e99dc42d
Kingdon Barrett <kbarret8@nd.edu>
2016-10-27 11:37:19 -0400

    more green specs

--- spec/features/javascript_search_spec.rb
+++ spec/features/javascript_search_spec.rb
@@ -4,8 +4,16 @@ feature 'JavaScript search controller' do
     expect(page).not_to have_errors
   end
   it 'should use employee-lookup to retrieve a single job result', js: true do
-    pending "the spec for employee-lookup-gem has been broken"
-    raise Error
+    visit '/job-lookup/search'
+    fill_in 'nd_employee_lookup_first_name', with: 'B'
+    fill_in 'nd_employee_lookup_last_name', with: 'Murphy'
+    find('#b_nd_employee_lookup_find').click
+
+    selector = find('#nd_employee_lookup_select_employee')
+    selector.find('span.emp_sel_first', text: 'Barbara').click
+
+    job = find('#jobs_list .employee_job')
+    expect(job).to have_selector('.job_title', text: 'Surviving Spouse Administrator')
   end
   it 'should find a person with more than one active job', js: true do
     pending "the job-lookup is implemented abstractly and does not provide show"
