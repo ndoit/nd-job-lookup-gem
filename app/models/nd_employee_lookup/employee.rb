1066a5c6af4d4ce1cf3c903beeae9b603dd90178
Kingdon Barrett <kbarret8@nd.edu>
2016-11-03 14:52:39 -0400

    wishful thinking

--- app/models/nd_employee_lookup/employee.rb
+++
@@ -1,6 +0,0 @@
-module NdEmployeeLookup
-  class Employee < ActiveRecord::Base
-    has_many :employee_jobs, class_name: 'NdJobLookup::EmployeeJob'
-    accepts_nested_attributes_for :employee_jobs
-  end
-end
