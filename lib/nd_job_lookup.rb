44f940ded5a5a5b3eff73984bb04a26b9bd7a606
Kingdon Barrett <kbarret8@nd.edu>
2016-11-29 10:21:16 -0500

    convenience methods for extension of models

--- lib/nd_job_lookup.rb
+++ lib/nd_job_lookup.rb
@@ -1,4 +1,15 @@
 require "nd_job_lookup/engine"
 
 module NdJobLookup
+  def self.root
+    File.expand_path(File.dirname(File.dirname(__FILE__))
+  end
+
+  def self.models_dir
+    "#{root}/app/models/nd_job_lookup"
+  end
+
+  def self.controllers_dir
+    "#{root}/app/controllers/nd_job_lookup"
+  end
 end
